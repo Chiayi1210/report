74c317355ebcde98b42bb928b5061208fd439d05
TsuChun0971 <113504467+TsuChun0971@users.noreply.github.com>
2023-01-05 21:12:51 +0800

    Update DBConfig.java

--- Final/src/main/java/hitstd/group/tool/database/DBConfig.java
+++ Final/src/main/java/hitstd/group/tool/database/DBConfig.java
@@ -6,7 +6,7 @@ public class DBConfig {
 	
 	public String FilePath(){
 	   //DB FilePath
-		 String DBPath="//Users/chenzujun/Desktop/report/Final/src/main/webapp/member.accdb";
+		 String DBPath="C:\\Users\\鄭嘉儀\\OneDrive\\桌面\\作業\\程式\\report\\Final\\src\\main\\webapp\\member.accdb";
 	   return DBPath;		
 	}
 }
